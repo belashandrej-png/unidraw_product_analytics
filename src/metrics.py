dc6b4938464a39a37df5b8f0b20ad2a2836837b5
Andrei <belashandrej@gmail.com>
2026-07-28 00:07:25 +0300

    Create metrics.py

---
+++ src/metrics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+def calculate_retention(visits_df, users_df, group_col='group_name'):
+    """Расчет retention по неделям"""
+    # Код из notebook 3
+    pass
+
+def calculate_mde(n1, n2, std1, std2, alpha=0.05, power=0.80):
+    """Расчет минимального обнаружимого эффекта"""
+    from statsmodels.stats.power import TTestIndPower
+    # Код расчета MDE
+    pass
